ed6e953ec12961064f3b940bed76f2cde737a3a6
samhine <sam.hine27@gmail.com>
2019-12-02 11:41:45 +0000

    recursion on day1

--- 1/second.py
+++ 1/second.py
@@ -1,12 +1,8 @@
 with open('input.txt', 'r') as f:
     inputtxt = [int(line.strip()) for line in f]
 
-s = 0
-for module in inputtxt:
-    fuel = module
-    while int(fuel/3)-2>0:
-        n_fuel = int(fuel/3)-2
-        s += n_fuel
-        fuel = n_fuel
+def f(m):
+    if (int(m/3)-2<0): return m
+    else: return f(int(m/3)-2)+m
 
-print(s)
+print(sum([f(module)-module for module in inputtxt]))
